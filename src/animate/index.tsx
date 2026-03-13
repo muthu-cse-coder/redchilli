3a65d6871b4b098b6bb981cc2c28d43759dbb71b
malathi-deve <malathimuthusamy2004@gmail.com>
2026-03-13 16:33:27 +0530

    update code

--- src/animate/index.tsx
+++ src/animate/index.tsx
@@ -3,14 +3,14 @@ import style from './index.module.scss';
 
 const Animate = () => {
     const images = [
-        "public/683333f2-729a-4ab0-88df-08ef35eb03c5.jfif",
-        "public/e448edee-fb65-403d-98db-dcf687530646.jfif",
-        "public/070a349f-2444-4541-9375-53fa2de58b83.jfif",
-        "public/805a4593-b9d9-4c0f-9653-e5cce600b5a6.jfif",
-        "public/7ceae2ef-eed8-47f3-8f88-42e9eb19b23e.jfif",
-        "public/34cbc7ea-fe8c-4a90-b33e-3d4040c97db0.jfif",
-        "public/03a8bde8-874d-42b7-b9dd-bba8bb16af87.jfif",
-        "public/ebd8132c-d9e2-471c-92ff-961ab1629748.jfif"
+        "/683333f2-729a-4ab0-88df-08ef35eb03c5.jfif",
+        "/e448edee-fb65-403d-98db-dcf687530646.jfif",
+        "/070a349f-2444-4541-9375-53fa2de58b83.jfif",
+        "/805a4593-b9d9-4c0f-9653-e5cce600b5a6.jfif",
+        "/7ceae2ef-eed8-47f3-8f88-42e9eb19b23e.jfif",
+        "/34cbc7ea-fe8c-4a90-b33e-3d4040c97db0.jfif",
+        "/03a8bde8-874d-42b7-b9dd-bba8bb16af87.jfif",
+        "/ebd8132c-d9e2-471c-92ff-961ab1629748.jfif"
     ];
     return (
         <div id="demos" className={style.stHoleImageWrapper}>
